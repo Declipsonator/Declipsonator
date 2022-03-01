154fcecbf4e9a060d139a21d77c33f77afa3bd43
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-03-01 17:12:05 -0500

    poggers

--- readme_update.py
+++ readme_update.py
@@ -1,17 +1,21 @@
 import requests
 from github import Github
+from datetime import datetime
+from os import getenv
+from pytz import timezone
+
 saved_projects = []
+g = Github('s', getenv('ACCESS_TOKEN'))
 
 
 def get_github_downloads(user, save_projects):
     counted_downloads = 0
-    g = Github()
 
     user = g.get_user(user)
 
     for repo in user.get_repos():
-        if save_projects & repo.stargazers_count >= 1:
-            saved_projects.add(repo.name)
+        if save_projects and repo.stargazers_count >= 1:
+            saved_projects.append(repo.name)
         for release in repo.get_releases():
             for asset in release.get_assets():
                 counted_downloads += asset.download_count
@@ -19,7 +23,7 @@ def get_github_downloads(user, save_projects):
     return counted_downloads
 
 
-def get_modrinth_downloads(user, save_projects):
+def get_modrinth_downloads(user):
     counted_downloads = 0
     url = "https://api.modrinth.com/v2/user/{}/projects".format(user)
     response = requests.get(url).json()
@@ -30,7 +34,7 @@ def get_modrinth_downloads(user, save_projects):
     return counted_downloads
 
 
-def get_curseforge_downloads(user, save_projects):
+def get_curseforge_downloads(user):
     counted_downloads = 0
     url = "https://api.cfwidget.com/author/search/{}".format(user)
     response = requests.get(url).json()
@@ -45,17 +49,28 @@ def get_curseforge_downloads(user, save_projects):
     return counted_downloads
 
 
+def get_github_projects_string(projects, user):
+    project_string = ''
+    for project in projects:
+        project_string += '- [{}](https://github.com/{}/{})\n'.format(project.replace('-', ' '), user, project)
+    return project_string
+
+
 total_downloads = 0
 
-total_downloads += get_github_downloads('Declipsonator', true)
-total_downloads += get_modrinth_downloads('Declipsonator', false)
-total_downloads += get_curseforge_downloads('dexlips', false)
-
-project_string = ''
-for project in saved_projects:
-    project_string += '- [{}]({})\n'.format(project)
+total_downloads += get_github_downloads('Declipsonator', True)
+total_downloads += get_modrinth_downloads('Declipsonator')
+total_downloads += get_curseforge_downloads('dexlips')
 
 template = requests.get('https://raw.githubusercontent.com/Declipsonator/Declipsonator/main/template.md').text
 
-print(template.replace('{downloads}', str(total_downloads)).replace('{projects}', project_string))
-            
+tz = timezone('EST')
+template = template.replace('{downloads}', str(total_downloads))\
+    .replace('{projects}', get_github_projects_string(saved_projects, 'Declipsonator'))\
+    .replace('{last_updated}', datetime.now().strftime('%Y-%m-%d %H:%M (EST)'))
+
+
+
+with open('README.MD', 'w', encoding='UTF-8') as f:
+    f.write(template)
+    f.close()
