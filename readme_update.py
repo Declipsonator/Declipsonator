2730059dcf1b6a6925519e089ea269345a06b5bb
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-03-02 21:25:09 -0500

    Update readme_update.py

--- readme_update.py
+++ readme_update.py
@@ -4,22 +4,26 @@ from datetime import datetime
 from os import getenv
 
 saved_projects = []
-saved_descriptions = []
+download_count = []
 g = Github('s', getenv('ACCESS_TOKEN'))
 
 
-def get_github_downloads(user, save_projects):
+def get_github_downloads(user):
     counted_downloads = 0
 
     user = g.get_user(user)
 
     for repo in user.get_repos():
-        if save_projects and repo.stargazers_count >= 1:
-            saved_projects.append(repo.name)
-            saved_descriptions.append(repo.description)
+        repo_download_count = 0
+
         for release in repo.get_releases():
             for asset in release.get_assets():
                 counted_downloads += asset.download_count
+                repo_download_count += asset.download_count
+
+        saved_projects.append(
+            [repo.name, repo.description, repo.stargazers_count, repo.url, repo_download_count, 'Github'])
+        download_count.append(repo_download_count)
 
     return counted_downloads
 
@@ -31,6 +35,11 @@ def get_modrinth_downloads(user):
 
     for mod in response:
         counted_downloads += mod.get('downloads')
+        saved_projects.append(
+            [mod.get('title'), mod.get('description'), 0,
+             'https://modrinth.com/' + mod.get('project_type') + '/' + mod.get('slug'), mod.get('downloads'),
+             'Modrinth'])
+        download_count.append(mod.get('downloads'))
 
     return counted_downloads
 
@@ -46,31 +55,57 @@ def get_curseforge_downloads(user):
         url = "https://api.cfwidget.com/{}".format(project_id)
         response = requests.get(url).json()
         counted_downloads += response.get('downloads').get('total')
+        for gm_project in saved_projects:
+            if gm_project[0].strip() == response.get('title').strip():
+                og_downs = gm_project[4]
+                gm_project[4] = gm_project[4] + response.get('downloads').get('total')
+                for i in range(0, len(download_count)):
+                    if download_count[i] == og_downs:
+                        download_count[i] = og_downs + response.get('downloads').get('total')
+
+
 
     return counted_downloads
 
 
 def get_github_projects_string(projects, user):
     project_string = ''
-    i = 0
     for project in projects:
-        project_string += '- [{}](https://github.com/{}/{}) - {}\n'.format(project.replace('-', ' '), user, project, saved_descriptions[i])
-        i += 1
+        if project[2] >= 1:
+            project_string += '- [{}](https://github.com/{}/{}) - {}\n'.format(project[0].replace('-', ' '), user,
+                                                                               project[0], project[1])
     return project_string
+
+
+def get_most_downloaded_string(projects):
+    downloaded_string = ''
+    download_count.sort(reverse=True)
+    print(download_count)
+    for project in projects:
+        try:
+            for i in range(0, 3):
+                if project[4] == download_count[i]:
+                    downloaded_string += '- {} - {} downloads  \n'.format(project[0].replace('-', ' '), project[4])
+                    download_count[i] = -1
+                    break
+        except:
+            ''
+
+    return downloaded_string
 
 
 total_downloads = 0
 
-total_downloads += get_github_downloads('Declipsonator', True)
+total_downloads += get_github_downloads('Declipsonator')
 total_downloads += get_modrinth_downloads('Declipsonator')
 total_downloads += get_curseforge_downloads('dexlips')
 
 template = requests.get('https://raw.githubusercontent.com/Declipsonator/Declipsonator/main/template.md').text
 
-template = template.replace('{downloads}', str(total_downloads))\
-    .replace('{projects}', get_github_projects_string(saved_projects, 'Declipsonator'))\
-    .replace('{last_updated}', datetime.utcnow().strftime('%Y-%m-%d %H:%M (UTC)'))
-
+template = template.replace('{downloads}', str(total_downloads)) \
+    .replace('{projects}', get_github_projects_string(saved_projects, 'Declipsonator')) \
+    .replace('{last_updated}', datetime.utcnow().strftime('%Y-%m-%d %H:%M (UTC)')) \
+    .replace('{top_three}', get_most_downloaded_string(saved_projects))
 
 
 with open('README.md', 'w', encoding='UTF-8') as f:
